43fae16498e17204b78a8ec678260e3bf741254d
Tran Quan Trung <gnurt10@gmail.com>
2018-08-31 16:11:48 +0700

    first commnit

--- server.js
+++ server.js
@@ -15,7 +15,21 @@ server.get("/product", (req, res) => {
 server.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html")
 })
-
+server.get("/blog", (req, res) => {
+  res.sendFile(__dirname + "/public/blog.html")
+})
+server.get("/account", (req, res) => {
+  res.sendFile(__dirname + "/public/account.html")
+})
+server.get("/detailblog", (req, res) => {
+  res.sendFile(__dirname + "/public/detailblog.html")
+})
+server.get("/category", (req, res) => {
+  res.sendFile(__dirname + "/public/category.html")
+})
+server.get("/cart", (req, res) => {
+  res.sendFile(__dirname + "/public/cart.html")
+})
 server.listen(server.get('port'), (req, res) => {
   console.log("Server is running on port %s...", server.get('port'));
 })
